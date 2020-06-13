182841a5c2a9cc44f75a41b5ae14acbad840a724
Zach Delano <z.delano@outlook.com>
2020-06-13 17:45:41 -0600

    102 - change folder structure

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ServerStatusIndex from './ServerStatusIndex.vue'
-import ServerStatusHeader from './ServerStatusHeader.vue'
-import ServerStatusFooter from './ServerStatusFooter.vue'
+import ServerStatusIndex from './components/server/ServerStatusIndex.vue'
+import ServerStatusHeader from './components/shared/ServerStatusHeader.vue'
+import ServerStatusFooter from './components/shared/ServerStatusFooter.vue'
 
 Vue.component('server-status-index', ServerStatusIndex)
 Vue.component('server-status-header', ServerStatusHeader)
