b86044ec13d52bc30c18972510e66e05fb6cd238
Zach Delano <z.delano@outlook.com>
2020-06-13 18:00:17 -0600

    105 - scope styles

--- src/components/server/ServerStatusIndex.vue
+++ src/components/server/ServerStatusIndex.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="row">
-        <div class="col-xs-12 col-sm-6">
+        <div class="col-xs-12 col-sm-6 index">
             <ul class="list-group">
                 <server-status v-for="index in 5" :index="index"></server-status>
             </ul>
         </div>
-        <div class="col-xs-12 col-sm-6">
+        <div class="col-xs-12 col-sm-6 details">
             <p>Server Details are currently not updated</p>
         </div>
     </div>
@@ -18,3 +18,11 @@ export default {
     }
 }
 </script>
+<style scoped lang="css">
+    div.details {
+        border: 1px solid red;
+    }
+    div.index {
+        border: 1px solid blue;
+    }
+</style>
